69034c7d48fc8fa3faf7723b57d1645f9898a364
Ken Takahashi <takaken@redimpulz.com>
2018-11-23 02:17:52 +0900

    設定を修正

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=!(5$dfl-8akieb=l6zj_0=yn@_#d*v)#5ix8=(2d1u8er34f$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
